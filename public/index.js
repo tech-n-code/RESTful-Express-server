a428eb0b759e09422ec0284aaf67bf8f7974fa84
Will Franceschini <>
2023-04-10 07:04:35 -0700

    minor edits; still need to incorporate C.U. and D

--- public/index.js
+++ public/index.js
@@ -1,9 +1,18 @@
-let container = document.querySelector(".container");
+console.log("Linked!")
+// document.addEventListener("DOMContentLoaded", function() {
 
-fetch("/api/pets")
-.then(response => response.json())
-.then(pets => {
-    pets.forEach(pet => {
-        container.innerHTML += `<h2>${pet.name}</h2>`;
+    let container = document.querySelector(".container");
+    
+    fetch("/api/pets")
+    .then(function (response) {
+        return response.json();
     })
-})
+    .then(function (pets) {
+        console.log(pets);
+        pets.forEach(function (pet) {
+            console.log("Adding h2 for pet:", pet);
+            container.innerHTML += `<h2>${pet.name}</h2>`;
+        });
+    });
+
+// });
--- server.js
+++ server.js
@@ -4,18 +4,26 @@ import pg from "pg";
 const { Client } = pg;
 
 const app = express();
-app.use(express.static("public"));  //<-- right after `app` is created and before routes
+app.use(express.static("public"));
 
-dotenv.config();  //<-- has to be before 'process.env'
+dotenv.config();
 const port = process.env.PORT || 3000;
 
 const client = new Client(process.env.DATABASE_URL);
 client.connect();
 
-app.use(express.json());  //<--has to be before routes
+app.use(express.json());
 
-app.get("/", function(req, res) {   
+app.get("/test", function(req, res) {   
     res.send("Hello, world!");
+});
+
+app.get('/api/pets', (req, res) => {
+    client.query(`SELECT * FROM pet`, (err, response) => {
+        console.log(err ? err : response.rows);
+        res.json(response.rows);
+        client.end;
+    })
 });
 
 app.listen(port, function(err) {
